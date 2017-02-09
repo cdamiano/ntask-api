7b2459e7d0325bf1d1616fd058f4b92177257952
Con Damianopoulos <con.damianopoulos@rea-group.com>
2017-02-09 14:11:46 +1100

    added logs

--- libs/middlewares.js
+++ libs/middlewares.js
@@ -1,9 +1,22 @@
 import bodyParser from "body-parser"
 import express from "express"
+import cors from "cors"
+import morgan from "morgan"
+import logger from "./logger.js"
 
 module.exports = app => {
   app.set("port", 3000);
   app.set("json spaces", 4);
+  app.use(morgan("common", {
+    stream: {
+      write: (message) => { logger.info(message); }
+    }
+  }));
+  app.set(cors({
+    origin: ["http://localhost:3001"],
+    methods: ["GET", "POST", "PUT", "DELETE"],
+    allowedHeaders: ["Content-Type", "Authorization"]
+  }));
   app.use(bodyParser.json());
   app.use(app.auth.initialize());
   app.use((req,res,next) => {
